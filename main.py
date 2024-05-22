9e5f5614e728d684a86fd3ebf015fe88b423753e
Hirthik Balaji C <89502294+HirthikBalaji@users.noreply.github.com>
2024-05-22 13:57:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/create_file', methods=['POST'])
+def create_file():
+    user_input = request.form['user_input']
+    filename = 'user_input.txt'
+    
+    # Write user input to a text file on the server
+    with open(filename, 'w') as file:
+        file.write(user_input)
+    
+    # Create a response with the file attachment
+    response = make_response(open(filename).read())
+    response.headers['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
